bfdeb24959dd346d51f05b492b51d6495311e16a
Artem <mirraxgg@gmail.com>
2024-10-23 16:41:33 +0300

    Update and Build

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use chrono::{DateTime, Utc};
+use rdev::{grab, Event, EventType, Key};
+use std::env;
+use std::fs;
+use xcap::Monitor;
+
+const TARGET_DIR: &str = "screens";
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let screens_dir = args.get(1).unwrap_or(&TARGET_DIR.to_string()).to_string();
+    let mut path = env::current_dir()?;
+    path.push(&screens_dir);
+
+    fs::create_dir_all(path)?;
+
+    if let Err(error) = grab(move |e| callback(e, &screens_dir)) {
+        println!("Error: {error:?}");
+    }
+
+    Ok(())
+}
+
+fn callback(event: Event, screens_dir: &String) -> Option<Event> {
+    if is_printscreen(&event) {
+        make_screen(screens_dir);
+        None
+    } else {
+        Some(event)
+    }
+}
+
+fn make_screen(screens_dir: &String) {
+    let monitors = Monitor::all().unwrap();
+
+    for monitor in monitors {
+        let image = monitor.capture_image().unwrap();
+
+        let now: DateTime<Utc> = Utc::now();
+
+        image
+            .save(format!(
+                "{}/{}-{}.png",
+                screens_dir,
+                now.format("%d-%m-%Y_%H_%M_%S"),
+                normalized(monitor.name())
+            ))
+            .unwrap();
+    }
+}
+
+fn normalized(filename: &str) -> String {
+    filename.replace(['|', '\\', ':', '/'], "")
+}
+
+#[cfg(not(target_os = "macos"))]
+fn is_printscreen(event: &Event) -> bool {
+    matches!(event.event_type, EventType::KeyPress(Key::PrintScreen))
+}
+
+#[cfg(target_os = "macos")]
+fn is_printscreen(event: &Event) -> bool {
+    matches!(event.event_type, EventType::KeyPress(Key::F9))
+}
